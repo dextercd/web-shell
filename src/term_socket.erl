025d01e437b9644f00e957749acda4cc04342d36
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-07-25 22:03:16 +0200

    Fix timeout on Chrome after 5 minutes

--- src/term_socket.erl
+++ src/term_socket.erl
@@ -5,7 +5,13 @@
 -export([init/2, websocket_init/1, websocket_handle/2, websocket_info/2, terminate/3]).
 
 init(Req, _State) ->
-    {cowboy_websocket, Req, no_terminal}.
+    % Set timeout to 2 minutes.
+    % This is necessary because Chrome puts pages in 'low power' mode after 5
+    % minutes of inactivity. In low power mode (JavaScript) timers are checked
+    % every minute, instead of regularly. This means that the default of 60
+    % seconds won't work well on Chrome.
+    {cowboy_websocket, Req, no_terminal,
+        #{idle_timeout => 2 * 60 * 1000}}.
 
 websocket_init(no_terminal) ->
     Terminal = pty_manager_server:create_terminal(),
