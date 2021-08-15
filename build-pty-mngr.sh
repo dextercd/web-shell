6fd72ca4aed7e08a8f5412413e317a2269254eea
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-08-15 15:59:40 +0200

    Don't reassign *BUILD_TYPE vars

--- build-pty-mngr.sh
+++ build-pty-mngr.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-effective_build_type="${PTY_BUILD_TYPE:=${BUILD_TYPE:=debug}}"
+effective_build_type="${PTY_BUILD_TYPE:-${BUILD_TYPE:-debug}}"
 build_dir="pty_mngr_build_${effective_build_type}"
 
 cmake --build "$build_dir"
--- build-wasm-terminal.sh
+++ build-wasm-terminal.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-effective_build_type="${WTERM_BUILD_TYPE:=${BUILD_TYPE:=debug}}"
+effective_build_type="${WTERM_BUILD_TYPE:-${BUILD_TYPE:-debug}}"
 build_dir="wasm_terminal_build_${effective_build_type}"
 
 cmake --build "$build_dir" --target wasm-terminal
